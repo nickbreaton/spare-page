0299996d654d83dcd24d4dc453cadc7429aeca1c
nickbreaton <nick@breaton.com>
2017-03-12 15:21:10 -0400

    Fix CORS issue

--- functions/src/enable-cors.js
+++ functions/src/enable-cors.js
@@ -1,4 +1,5 @@
 export default function (res) {
   res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
 }
