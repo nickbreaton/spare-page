c62ac5ee2f7f733afdef092b690ce70353a2d39a
nickbreaton <nick@breaton.com>
2017-04-02 18:43:31 -0400

    Fix IE dropzone issue

--- client/src/components/Drop.js
+++ client/src/components/Drop.js
@@ -39,6 +39,8 @@ class Drop extends Component {
           style={{
             top: '0',
             bottom: '0',
+            left: '0',
+            right: '0',
             width: '100%',
             height: '100%',
             position: 'absolute'
